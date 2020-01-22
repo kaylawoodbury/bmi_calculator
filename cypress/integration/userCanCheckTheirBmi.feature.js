b044492bcd52bb8d86adc7dce847b89600d1b43c
kaylawoodbury <kaylasandoval92@gmail.com>
2020-01-22 17:04:01 +0100

    creating first test for user to enter weight and height

---
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -0,0 +1,8 @@
+ /* This is a test to see if the user can access the page to check their bmi*/
+
+ describe(
+     'User can check their BMI', () => {
+    it ('user can enter weight and height', () => {
+        cy.visit('http://localhost:3001')
+    })
+} )
