fc6c1c8c9e87d274cbd74bc99904a446e115e9af
kaylawoodbury <kaylasandoval92@gmail.com>
2020-01-22 17:32:55 +0100

    adding test for a bmi value to be calculated and displayed on the page

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -9,5 +9,8 @@
         cy.get('#height').type('186')
         cy.get('#Calculate').click()
     })
+    it ('displays a BMI value of 26.01', () => {
+        cy.get('#results').should('contain', '26.01')
+    })
 } )
 
