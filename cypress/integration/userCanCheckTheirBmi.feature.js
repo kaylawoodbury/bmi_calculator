d02e8503e1f6bd5e68ed1cdb1806c5d1f607adfe
kaylawoodbury <kaylasandoval92@gmail.com>
2020-01-22 17:16:46 +0100

    adding second test to visit the site and verify it contains BMI Calculator

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -4,5 +4,7 @@
      'User can check their BMI', () => {
     it ('user can enter weight and height', () => {
         cy.visit('http://localhost:3001')
+        cy.contains('BMI Calculator')
     })
 } )
+
